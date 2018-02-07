f2f83c22ddf06ae63e6e74586c68d4b01e8ce346
hondamadeline <hondamadeline@gmail.com>
2018-02-07 14:13:53 -0500

    message about this commit like first try or update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   
-  
+  resources :users
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
+ root 'static_pages#home'
 
 end

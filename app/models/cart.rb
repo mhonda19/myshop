c950f9c59f06bd535ffe21daeb272b07d96aea3a
hondamadeline <hondamadeline@gmail.com>
2018-02-26 15:53:37 -0500

    update to functioning app repository

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
     if current_item
@@ -9,7 +10,17 @@ class Cart < ApplicationRecord
     end
     current_item
   end
+  
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
+  
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+    item.cart_id = nil
+    line_items << item
+    end
+  end
+
+  
 end
